93180fc0f70353b1558851ee7cf17299761c3565
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-02-24 22:52:15 -0800

    drug amount data structure filling functionality complete, and export to CSV working. must wait a while for it to iterate, though.

--- dataHolder.js
+++ dataHolder.js
@@ -4,17 +4,25 @@ This file holds some global variables for
 storing the data we scrape
 */
 
-
+//for storing and grabbing source code that the ajax
+//request gets in sourceCodeGetter.js
 var sourceCode = "";
 
+//for holding option values so we can perform
+//search
 var OptionValues_Drugs = [];
 
+//for holding total amount of reports for each
+//drug
+var AmtOfReports_Drugs = [];
 
 
 //Holds the total amount of reports for each drug.
 //Used so we can locate the top 10-15 most used drugs.
 var drugTotalsList = [];
-var drugTotalsListEntry = {
-    drug: "",
-    amount: 0
-};
+//var drugTotalsListEntry = {
+//    drug: "",
+//    amount: 0
+//};
+
+var amountList = [];
--- scraper.js
+++ scraper.js
@@ -68,13 +68,169 @@ function collectTotals() {
     //code above is janky, so i have to remove the first two indexes,
     //which are junk
     OptionValues_Drugs.splice(0, 2);
-    
-    console.log(OptionValues_Drugs[0].drug + ": " + OptionValues_Drugs[0].optionValue + ", array length: " + OptionValues_Drugs.length);
+
+    //        console.log(OptionValues_Drugs[34].drug + ": " + OptionValues_Drugs[34].optionValue + ", array length: " + OptionValues_Drugs.length);
+
+    fillUpTotals();
+
+
+    function fillUpTotals() {
+        var urlFields = []; //for constructing the URL
+        var allURLS = []; //holds all the URLs we construct using the option values
+        var result = []; //holds the IDs we get. we use the length to know amount of reports.
+
+        //iterate through all the drugs
+        for (var i = 0; i < OptionValues_Drugs.length; i++) {
+            var drugOne = OptionValues_Drugs[i].optionValue;
+            var drugTwo = -1;
+            var drugThree = -1;
+            var category = -1;
+            var nonSubstance = -1;
+            var gender = -1;
+            var context = -1;
+            var doseMethod = -1;
+            var title = "";
+            var authorSearch = "";
+            var erowidAuthor = -1;
+            var language = 1;
+            var group = -1;
+            var strength = "";
+            var intensityMin = "";
+            var intensityMax = "";
+
+            var drugTotalsListEntry = {
+                drug: "",
+                amount: 0
+            };
+
+            drugTotalsListEntry.drug = OptionValues_Drugs[i].drug;
+
+            drugTotalsList.push(drugTotalsListEntry);
+
+
+            function setupURL(f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15, f16) {
+                urlFields = {
+                    init: "https://www.erowid.org/experiences/exp.cgi?A=Search",
+                    drugOne: "&S1=" + f1,
+                    // rest: "&S2=-1&S3=-1&C1=-1&S4=-1&GenderSelect=-1&Context=\
+                    // -1&DoseMethodID=-1&Title=&AuthorSearch=&A1=-1&Lang=1&Grou\
+                    // p=-1&Str=&Intensity=&I2="
+                    drugTwo: "&S2=" + f2,
+                    drugThree: "&S3=" + f3,
+                    category: "&C1=" + f4,
+                    nonSubstance: "&S4=" + f5,
+                    gender: "&GenderSelect=" + f6,
+                    context: "&Context=" + f7,
+                    doseMethod: "&DoseMethodID=" + f8,
+                    title: "&Title=" + f9,
+                    authorSearch: "&AuthorSearch=" + f10,
+                    erowidAuthor: "&A1=" + f11,
+                    language: "&Lang=" + f12,
+                    group: "&Group=" + f13,
+                    strength: "&Str=" + f14,
+                    intensityMin: "&Intensity=" + f15,
+                    intensityMax: "&I2=" + f16
+                }
+            }
+
+            setupURL(drugOne,
+                drugTwo,
+                drugThree,
+                category,
+                nonSubstance,
+                gender,
+                context,
+                doseMethod,
+                title,
+                authorSearch,
+                erowidAuthor,
+                language,
+                group,
+                strength,
+                intensityMin,
+                intensityMax);
+
+            url = "";
+
+
+            for (var urlFields_index in urlFields) {
+                if (urlFields.hasOwnProperty(urlFields_index)) {
+                    url += urlFields[urlFields_index];
+                }
+            }
+
+            url += "&ShowViews=0&Start=0&Max=99999";
+
+            allURLS.push(url);
+
+
+        } //end for loop
+
+        //iterator for recursive loop
+        var thing = 0;
+        dood(thing);
+
+        
+        function dood(thang) {
+            //if we've iterated over all of them, stop
+            if (thang > allURLS.length) {
+                console.log("filled");
+                return;
+            }
+            
+            //get the source for the search, count the reports, and shove them
+            //with their appropriate drug in the drugTotalsList array
+            getSource(allURLS[thang], function () {
+                var text = sourceCode;
+
+                result = [];
+
+                var pos = text.indexOf('exp.php?ID');
+
+                result.push(text.substring(pos, pos + 24).replace(/(^.+\D)(\d+)(\D.+$)/i, '$2'));
+
+
+                while (pos != -1) {
+                    pos = text.indexOf('exp.php?ID', pos + 1);
+                    result.push(text.substring(pos, pos + 24).replace(/(^.+\D)(\d+)(\D.+$)/i, '$2'));
+
+                }
+
+                //this pop gets rid of last entry which ends
+                //up being "<html><head><met" or something weird
+                result.pop();
+
+                drugTotalsList[thang].amount = result.length;
+                console.log(drugTotalsList[thang].drug + " has " + drugTotalsList[thang].amount + " reports. " /* + "source parse: " + thang + ". URL: " + allURLS[thang]*/);
+                thing++;
+                dood(thing);
+
+            });
+        }
+
+
+
+
+
+
+
+
+
+
+    } //end fillUpTotals()
+
+
 }
 
 
 
 
+
+
+
+
+
+//old and will be cannibalized and deleted eventually
 function performScrape() {
     var urlFields;
 
