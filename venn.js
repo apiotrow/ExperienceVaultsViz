9a2ebf31d1c0162a2928cb37707dafc7bf03dc28
kagz <sealittl@ucsc.edu>
2015-03-12 16:13:01 -0700

    Update venn.js
    added sorting position based on csv

--- venn.js
+++ venn.js
@@ -5,17 +5,23 @@
 
  //Define Scales   
     var xScale = d3.scale.linear()
-        .domain([0,16]) //Need to redefine this after loading the data
+        //.domain([0,16]) //Need to redefine this after loading the data
         .range([0, width]);
 
     var yScale = d3.scale.linear()
-        .domain([-10,400]) //Need to redfine this after loading the data
+        //.base(0.5)
+        //.domain([-10,400]) //Need to redfine this after loading the data
         .range([height, 0]);
+
+    var xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickSize(-height);
+    var yAxis = d3.svg.axis().scale(yScale).orient("left").tickSize(-width);
     
     //Define Tooltip here
     var tooltip = d3.select("body").append("div")
         .attr("class", "tooltip")
         .style("opacity", 0);
+
+   
 
     var amountThreshold = 0;
 
@@ -26,17 +32,14 @@ d3.select("input").on('change', function() {
     else amountThreshold = 0;
 });
 
-//d3.select('button').on('click', function() {
+ 
+
+
+d3.select('button').on('click', function() {
      //Define SVG
-      d3.select("svg").remove();
-      var svg = d3.select("body")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
        //Get Data
-    d3.csv("drugamounts.csv", function(error,data) {
+    d3.csv("topdrugdatapercentages.csv", function(error,data) {
          //Define Color
         var colors = d3.scale.category20();
         colors.domain(d3.keys(data[0]).filter(function(key) { return key !== "drug"; }));
@@ -44,8 +47,53 @@ d3.select("input").on('change', function() {
             d.drug = d.drug;
             if (d.amount > amountThreshold)  {d.amount = +d.amount;}//filter out less popular drugs if checkbox is selected
             else {d.amount = +0;}
+            d.badtrip = +d.badtrip;
+            d.mystical = +d.mystical;
+            d.addiction = +d.addiction;
+            d.alone = +d.alone;
+            d.smallgroup = +d.smallgroup;
+            d.largegroup = +d.largegroup;
                 
         });
+        
+        xScale.domain([d3.min(data, function(d) { return d.mystical - d.badtrip; }), d3.max(data, function(d) { return d.mystical - d.badtrip; })]);
+        yScale.domain([d3.min(data, function(d) { return 100 - d.addiction; }), d3.max(data, function(d) { return 100 -d.addiction; })]);
+        
+
+       
+      var zoom = d3.behavior.zoom()
+        .x(xScale)
+        .y(yScale)
+        .scaleExtent([1, 32])
+        .on("zoom", zoomed);  
+        
+    var svg = d3.select("body")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .call(zoom);
+        
+        svg.append("rect")
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", width)
+            .attr("height", height)
+            .attr("fill", "lightgrey")
+            .attr("opacity", 0)
+            //.style("stroke-size", "1px");
+
+        
+     function zoomed() {
+            svg.selectAll(".dot")
+                .attr("cx", function(d) {return xScale(d.mystical - d.badtrip);})
+                .attr("cy", function(d) {return yScale(100- d.addiction);})
+            svg.select(".x.axis").call(xAxis);
+            svg.select(".y.axis").call(yAxis);
+    }
+      //d3.select("svg").remove();
+
         
     //Draw Scatterplot
     svg.selectAll("dot")
@@ -53,10 +101,11 @@ d3.select("input").on('change', function() {
         .enter().append("circle")
         .attr("class", "dot")
         .attr("r", function(d) { return Math.sqrt(d.amount)/.4; })
-        .attr("cx", function(d) {return Math.random() * 950;})
-        .attr("cy", function(d) {return Math.random() * 650;})
+        .attr("cx", function(d) {return xScale(d.mystical - d.badtrip); })
+        .attr("cy", function(d) {return yScale(100- d.addiction); })
         .style("fill", function (d) { return colors(d.drug); })
         .on("click", function(d) {
+           /*var name = d.drug;
            d3.select("svg").remove();
             var svg = d3.select("body")
                 .append("svg")
@@ -124,8 +173,8 @@ d3.select("input").on('change', function() {
                     tooltip.transition()
                     .duration(500)
                     .style("opacity", 0);
-                }); 
-        })
+                }); */
+        }) 
             //Add .on("mouseover", .....
         .on("mouseover", function(d) {
             tooltip.transition()
@@ -142,5 +191,7 @@ d3.select("input").on('change', function() {
                 .duration(500)
                 .style("opacity", 0);
         });   
+        
     });
-//});
+    
+});
