15b0e64770bdf5e1326b5f2225284587d863c069
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-07-23 23:18:56 -0700

    all values from the CSV now collected

--- csvthings.js
+++ csvthings.js
@@ -30,9 +30,8 @@ function readTextFile(file, callback) {
 function fillInDataStructure(result) {
     var csvText = result;
     var reports = csvText.split(/\r/); //array where each entry is a single report
-    var druglist = []; //array of drug names
-    var freq = {}; //final drug frequency count
-    var drugsInEachEntry = [];
+    var reportArray = [];
+    var drugList = []; //list of drug names
 
     //    getSourceCode("https://www.erowid.org/experiences/exp_search.cgi", function () {
     //        scrapeAllOptionValues();
@@ -41,8 +40,6 @@ function fillInDataStructure(result) {
     //        }
     //    });
 
-
-    var ttt = [];
     //replace commas in nested entries
     for (var i = 1; i < /*reports.length*/ 50; i++) {
 
@@ -71,77 +68,96 @@ function fillInDataStructure(result) {
             //check for another set of quotes in the entry
             firstQuote = reports[i].indexOf('"');
         }
-        ttt.push(reports[i].split(","));
+        reportArray.push(reports[i].split(","));
+        //        console.log(reports[i].split(","));
+    }
+
+    for (var i = 0; i < reportArray.length; i++) {
+        //entry for one report
+        var idEntry = {
+            drugs: [],
+            categ: [],
+            nonsub: [],
+            context: [],
+            intensity: "",
+            gender: "",
+            title: "",
+            author: "",
+            date: "",
+            views: ""
+        }
+
+        //insert identry into complete data structure
+        complete[reportArray[i][0]] = idEntry;
+
+        //insert drugs for this report
+        var drugs = reportArray[i][1].split(";");
+        for (var j = 0; j < drugs.length; j++) {
+            //entry for the drug array in the completeEntry object ^
+            var drugEntry = {
+                drug: "",
+                method: "",
+                amount: "",
+                form: ""
+            }
+
+            if (drugs[j].indexOf("[") != -1) {
+                drugEntry.drug = drugs[j].substring(0, drugs[j].indexOf("["));
+            } else {
+                drugEntry.drug = drugs[j].substring(0);
+            }
+
+            if (drugs[j].indexOf("[method]") != -1) {
+                var text = drugs[j].substring(drugs[j].indexOf("[method]") + 8);
+                if (text.indexOf("[") != -1) {
+                    drugEntry.method = text.substring(0, text.indexOf("["));
+                } else {
+                    drugEntry.method = text.substring(0);
+                }
+            }
+
+            if (drugs[j].indexOf("[amount]") != -1) {
+                var text = drugs[j].substring(drugs[j].indexOf("[amount]") + 8);
+                if (text.indexOf("[") != -1) {
+                    drugEntry.amount = text.substring(0, text.indexOf("["));
+                } else {
+                    drugEntry.amount = text.substring(0);
+                }
+            }
+
+            if (drugs[j].indexOf("[form]") != -1) {
+                var text = drugs[j].substring(drugs[j].indexOf("[form]") + 6);
+                if (text.indexOf("[") != -1) {
+                    drugEntry.form = text.substring(0, text.indexOf("["));
+                } else {
+                    drugEntry.form = text.substring(0);
+                }
+            }
+//            console.log(drugEntry.drug + " " + drugEntry.method + " " + drugEntry.amount + " " + drugEntry.form);
+            complete[reportArray[i][0]].drugs.push(drugEntry);
+        }
+
+        //insert every other column for this report
+        complete[reportArray[i][0]].categ.push(reportArray[i][2].split(";"));
+        complete[reportArray[i][0]].nonsub.push(reportArray[i][3].split(";"));
+        complete[reportArray[i][0]].context.push(reportArray[i][4].split(";"));
+        complete[reportArray[i][0]].intensity = reportArray[i][6];
+        complete[reportArray[i][0]].gender = reportArray[i][7];
+        complete[reportArray[i][0]].title = reportArray[i][8];
+        complete[reportArray[i][0]].author = reportArray[i][9];
+        complete[reportArray[i][0]].date = reportArray[i][10];
+        complete[reportArray[i][0]].views = reportArray[i][11];
         
+        console.log(complete[reportArray[i][0]]);
     }
-    console.log(ttt);
+    
+    
 
-    //insert entries into complete structure
-    complete[reports[i][0]] = idEntry;
-    //complete[reports[i][1]].drugs = drugEntry;
-
-    //entry for one report
-    var idEntry = {
-        drugs: [],
-        categ: [],
-        nonsub: [],
-        context: [],
-        intensity: "",
-        gender: "",
-        title: "",
-        author: "",
-        date: "",
-        views: ""
+    for (var key in complete) {
+        if (complete.hasOwnProperty(key)) {
+            //            console.log(key);
+            console.log(complete[key].categ);
+        }
     }
 
-    //entry for the drug array in the completeEntry object ^
-    var drugEntry = {
-        drug: "",
-        method: "",
-        amount: "",
-        form: ""
-    }
-
-
-
-
-
-
-    //    //iterate through every entry in
-    //    for (var i = 0; i < drugsInEachEntry.length; i++) {
-    //        var drugs = drugsInEachEntry[i].split(",");
-    //        var drugName = "";
-    //
-    //        //iterate through every drug in the entry, including ones with only one drug
-    //        for (var j = 0; j < drugs.length; j++) {
-    //            if (drugs[j].indexOf("[") != -1) {
-    //                drugName = drugs[j].substring(0, drugs[j].indexOf("["));
-    //            } else {
-    //                drugName = drugs[j];
-    //            }
-    //            freq[drugName] = 1;
-    //        }
-    //    }
-    //
-    //    var keys = [];
-    //    var stop = 0
-    //    for (var key in freq) {
-    //        if (freq.hasOwnProperty(key)) {
-    //            console.log(key + ": " + freq[key]);
-    //        }
-    //        stop++;
-    //        if (stop == 100) break;
-    //    }
-
-    //    var array2 = 
-
-    //    $(document).ready(function () {
-    //        for(var i = 0; i < array.length; i++){
-    //            array[i]
-    //            $("#popularTable").append('dfgdfgdfg');
-    //        }
-    //        
-    //    });
-
-    //    console.log(array);
 }
