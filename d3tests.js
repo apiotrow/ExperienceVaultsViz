23b2909cbeaac0d826ffa43c5fcd450be9f8842d
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-07-27 08:23:34 -0700

    more d3. bar chart

--- d3tests.js
+++ d3tests.js
@@ -1,42 +1,129 @@
 window.onload = function () {
 
-    for(var i = 0; i < 10; i++){
+
+    d3.csv("csvs/categprofilestrimmed.csv", function (error, data) {
+        console.log(data);
+        dataViz(data);
+    });
+
+    function dataViz(incomingData) {
+//        d3.select("body").selectAll("div.drug")
+//            .data(incomingData)
+//            .enter()
+//            .append("div")
+//            .attr("class", "drug")
+//            .html(function (d, i) {
+//                //                console.log(d["Mystical Experiences"]);
+//                return d["Mystical Experiences"];
+//            });
+
+        var maxPopulation = d3.max(incomingData, function (el) {
+            return parseInt(el["Mystical Experiences"]);
+        });
+
+        var yScale = d3.scale.linear().domain([0, maxPopulation]).range([0, 460]);
+        d3.select("svg").attr("style", "height: 480px; width: 600px;");
         d3.select("svg")
-        .append("circle")
-        .attr("r", i * 10)
-        .attr("cx", i * 40)
-        .attr("cy", i * 40)
-        .style("fill", "lightblue");
+            .selectAll("rect")
+            .data(incomingData)
+            .enter()
+            .append("rect")
+            .attr("width", 10)
+            .attr("height", function (d) {
+                return yScale(parseInt(d["Mystical Experiences"]));
+            })
+            .attr("x", function (d, i) {
+                return i * 10;
+            })
+            .attr("y", function (d) {
+                return 480 - yScale(parseInt(d["Mystical Experiences"]));
+            })
+            .style("fill", "blue")
+            .style("stroke", "red")
+            .style("stroke-width", "1px")
+            .style("opacity", .25);
     }
 
-    d3.select("svg")
-        .append("text")
-        .attr("id", "a")
-        .attr("x", 20)
-        .attr("y", 20)
-        .style("opacity", 0)
-        .text("fg fgddd");
-    d3.select("svg")
-        .append("circle")
-        .attr("r", 100)
-        .attr("cx", 400)
-        .attr("cy", 400)
-        .style("fill", "lightblue");
-    d3.select("svg")
-        .append("text")
-        .attr("id", "b")
-        .attr("x", 400)
-        .attr("y", 400)
-        .style("opacity", 0)
-        .text("dfgdfg");
-
-    d3.select("#a").transition().delay(500).style("opacity", 1);
-    d3.select("#b").transition().delay(1000).style("opacity", .3);
-    d3.selectAll("circle").transition().duration(1000).attr("cy", 200).attr("r", 10);
 
 
 
-    d3.select("#he").on("click", function () {
-        console.log("sdfsdf");
-    });
+
+
+    //        var yScale = d3.scale.linear()
+    //            .domain([0, 100, 6000])
+    //            .range([0, 100, 200])
+    //            .clamp(true);
+    //
+    //        d3.select("svg")
+    //            .selectAll("rect")
+    //            .data([14, 68, 24500, 430, 19, 1000, 5555])
+    //            .enter()
+    //            .append("rect")
+    //            .attr("width", 40)
+    //            .attr("height", function (d) {
+    //                return yScale(d);
+    //            })
+    //            .style("fill", "blue")
+    //            .style("stroke", "red")
+    //            .style("stroke-width", "1px")
+    //            .style("opacity", .25)
+    //            .attr("x", function (d, i) {
+    //                return i * 40;
+    //            })
+    //            .attr("y", function (d) {
+    //                return 500 - yScale(d);
+    //            });
+
+
+    //    console.log(document.getElementsByClassName("drug")[0].__data__);
+
+
+
+
+    //        d3.csv("csvs/categprofilestrimmed.csv", function (d) {
+    //            console.log(d)
+    //        });
+    //        console.log(Date.parse("Aug 22 1985"));
+    //        console.log(Date.parse("Aug 23 1985"));
+    //
+    //
+    //        for (var i = 0; i < 10; i++) {
+    //            d3.select("svg")
+    //                .append("circle")
+    //                .attr("r", i * 10)
+    //                .attr("cx", i * 40)
+    //                .attr("cy", i * 40)
+    //                .style("fill", "lightblue");
+    //        }
+    //
+    //        d3.select("svg")
+    //            .append("text")
+    //            .attr("id", "a")
+    //            .attr("x", 20)
+    //            .attr("y", 20)
+    //            .style("opacity", 0)
+    //            .text("fg fgddd");
+    //        d3.select("svg")
+    //            .append("circle")
+    //            .attr("r", 100)
+    //            .attr("cx", 400)
+    //            .attr("cy", 400)
+    //            .style("fill", "lightblue");
+    //        d3.select("svg")
+    //            .append("text")
+    //            .attr("id", "b")
+    //            .attr("x", 400)
+    //            .attr("y", 400)
+    //            .style("opacity", 0)
+    //            .text("dfgdfg");
+    //
+    //        d3.select("#a").transition().delay(500).style("opacity", 1);
+    //        d3.select("#b").transition().delay(1000).style("opacity", .3);
+    //        d3.selectAll("circle").transition().duration(1000).attr("cy", 200).attr("r", 10);
+    //
+    //
+    //
+    //        d3.select("#he").on("click", function () {
+    //            console.log("sdfsdf");
+    //        });
 }
