db980605f14c76b00bb9752dad62744a06cb9bea
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2016-07-23 06:03:13 -0700

    learning d3. rendering svgs. outputting some stats

--- bundle.js
+++ bundle.js
@@ -25880,17 +25880,84 @@ document.addEventListener('DOMContentLoaded', function () {
     };
 
     var testfile = statfiles.context_gender;
-    console.log(testfile);
+    // console.log(testfile);
 
     var t = getStats2(testfile, "Female", "perc", 10);
+    // console.log(t);
+
+    // t = getStats2(testfile, "Male", "perc", 10);
+    // console.log(t);
+
+    // testfile = statfiles.gender;
+    // t = getStats1(testfile, "perc", 10);
     console.log(t);
-    
-    t = getStats2(testfile, "Male", "perc", 10);
-    console.log(t);
+
+
+    var svgW = 600;
+    var svgH = 300;
+    var svg = d3.select('#vis').append('svg').attr('width',svgW).attr('height',svgH);
+    // svg.append('circle').style('stroke', 'red').style('fill', 'yellow').attr('r',50).attr('cx', svgW/2)
+    // .attr('cy', svgH/2);
+
+    var g = svg.append("svg:g");
+
+    var circ1 = circ(g, 40, 30, 15, 'blue', 'green');
+    var rect1 = rect(g, 50, 100, 10, 100);
+
+    rect1.attr('transform', 'translate(100, 60),scale(2, 1),rotate(30, 150, 100)');
+    // rect1.attr('transform', 'scale(2, 1)');
+    // rect1.attr('transform', 'rotate(30, 150, 100)');
+
+    circ1.transition().delay(100).duration(4000).attr('r',50).attr('cx', 90);
+
+    d3.select('svg').select('g').selectAll('rect').data(t).enter().append('rect')
+    .attr('width', 10).attr('x', function(d, i){return i * 10}).attr('h', function(d, i){return d[1] * 3})
+    .attr('y', 100);
+
+    function circ(obj, cx, cy, r, fill, outline){
+        return obj.append('circle').attr('r',r).attr('cx', cx).attr('cy', cy).style('fill', fill).style('stroke',outline);
+    }
+
+    function rect(obj, x, y, w, h){
+        return obj.append('rect').attr('x', x).attr('y', y).attr('width', w).attr('height', h);
+    }
+
+
+
+    d3.select('body').append('ul');
+    d3.select('ul').selectAll('li').data(t).enter().append('li').text(function(d){
+        return(d[0] + ": " + d[1]);
+    });
+
+
+
+
+    // var selection = d3.selectAll("div");
+    // selection.text("hey");
+
+    // d3.selectAll("div")
+    // .text("yoyo")
+    // .style('text-align','left')
+    // .append("p")
+    // .text("hi");
+
+    // d3.select("div").append('p').text("yes");
+
+    // d3.select('body').insert('div', 'div:nth-child(3)').text("yee");
+
+    // console.log(d3.selectAll("div").text());
+    // d3.select("h1").attr('align',"center").html('<h2>heyguys</h2>');
+
+    // d3.select('body').html("<h1>fuck you</h1>");
+
+
+
+
+
 
     //for statfiles with only 1 group
     //outputs sorted array for chosen stat (raw, perc, whatever)
-    function getStats1(file, stat)
+    function getStats1(file, stat, thresh)
     {
         var sortable=[];
         for(var key in file)
