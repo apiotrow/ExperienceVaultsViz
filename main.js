82a30f65b3787a05a091d6a12dd07c01bfccbfb7
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2016-10-27 06:52:56 -0700

    random category chosen for initial rendering. button area is always large enough to hold all buttons.

--- main.js
+++ main.js
@@ -66,21 +66,32 @@ document.addEventListener('DOMContentLoaded', function () {
     var buttonSVGH = 350;
     var buttonSVG = d3.select('#vis').append('svg').attr('width',buttonSVGW).attr('height',buttonSVGH).attr('id','buttonSVG');
 
-    
+    //choices that need to be buttons
     var statObject = statfiles.drug_category;
-    barGraphHoriz(statObject, eevv.categories.badTrip);
-    setupButtons(statObject);
+    var thresh = 200;
+
+    //find a random group for the graph so we have something to render initially
+    var keyNames = Object.keys(statObject);
+    var randomKey = keyNames[Math.floor(Math.random() * keyNames.length)];
+    keyNames = Object.keys(statObject[randomKey]);
+    randomKey = keyNames[Math.floor(Math.random() * keyNames.length)];
+    console.log(randomKey);
+
+    //render graph and buttons
+    barGraphHoriz(statObject, randomKey, thresh);
+    setupButtons(statObject, thresh);
 
     // var statObject = statfiles.context;
     // barGraphHoriz(statObject);
     // setupButtons(statObject);
 
-    function setupButtons(statObject){
+    function setupButtons(statObject, thresh){
     	var g = buttonSVG.append('svg:g');
     	var color = d3.scale.category20();
     	var buttonH = 40;
     	var buttonW = 80;
-    	var buttonSpacing = 10;
+    	var buttonSpacing = 50;
+    	var buttonRectPadding = 10;
 
     	var get = "";
     	for(key in statfiles){
@@ -89,7 +100,7 @@ document.addEventListener('DOMContentLoaded', function () {
 		    }    
 	    }
 
-    	var data = getStats1(get, "raw", 200);
+    	var data = getStats1(get, "raw", thresh);
 
         var lastTextEnd = 0;
         var currRow = 0;
@@ -102,13 +113,13 @@ document.addEventListener('DOMContentLoaded', function () {
         .text(function(d,i){return d[0]})
         .attr('x', function(d,i){
         	var textx = lastTextEnd;
-        	lastTextEnd += this.getComputedTextLength() + 30;
+        	lastTextEnd += this.getComputedTextLength() + buttonSpacing;
         	
         	//if this text is going off the edge of the SVG, start a new row
         	if(lastTextEnd > buttonSVGW){
         		currRow += 1;
         		textx = 0;
-        		lastTextEnd = this.getComputedTextLength() + 30;
+        		lastTextEnd = this.getComputedTextLength() + buttonSpacing;
         	}
         	
         	//set row property so we can adjust row Y later
@@ -146,7 +157,7 @@ document.addEventListener('DOMContentLoaded', function () {
         //create button rectangles using button dimenstions, etc
     	g.selectAll('rect').data(buttonLocations).enter().append('rect')
     	.attr('x', function(d,i){
-    		return d[0];
+    		return d[0] - buttonRectPadding;
     	})
     	.attr('y', function(d,i){
     		return d[1];
@@ -155,30 +166,38 @@ document.addEventListener('DOMContentLoaded', function () {
     		return d[3];
     	})
     	.attr('width', function(d,i){
-    		return d[2];
+    		return d[2] + buttonRectPadding * 2;
     	})
     	.attr('class','bar')
     	.attr('stroke','black')
     	.style('fill', function(d,i){return color(i)})
     	.on("click", function(d,i){
-        	var statObject = statfiles.drug_category;
-   			barGraphHoriz(statObject, d[4][i][0]);
+   			barGraphHoriz(statObject, d[4][i][0], thresh);
         });
+
+    	//make the button area SVG tall enough that it fits all the buttons
+    	var highestY = buttonLocations[buttonLocations.length - 1][1];
+    	var ynum = parseFloat(highestY);
+    	console.log(highestY);
+        buttonSVG.attr('height', ynum + 50);
 
     	//move text in front of buttons
     	g.selectAll('#buttontext').each(function(){
 	    	this.parentNode.appendChild(this);
 	  	});
+
+    	//shift button over so they aren't cut off on the left
+	  	g.attr('transform','translate(20,0)');
     }
 
 
 
-    function barGraphHoriz(statObject, stat){
+    function barGraphHoriz(statObject, stat, thresh){
     	svg.selectAll("*").remove(); //remove current graph
 
         // console.log(statObject);
         var rawOrPerc = "perc";
-        var data = getStats2(statObject, stat, rawOrPerc, 200);
+        var data = getStats2(statObject, stat, rawOrPerc, thresh);
         // var data = getStats1(statObject, rawOrPerc, 1);
 
         var g = svg.append('svg:g');
@@ -243,136 +262,9 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .attr('x', svgW / 2)
         .attr('y', 20)
-        .attr('text-anchor','middle');
+        .attr('text-anchor','middle')
+        .attr("font-family", "sans-serif");
     }
-
-
-/*
-    function barGraphOne(){
-        var g = svg.append("svg:g");
-        var margin_x = 32;
-        var margin_y = 20;
-
-        //BAR GRAPH
-        //---
-        var testfile = statfiles.context_gender;
-        var gender = "Female";
-        var data = getStats2(testfile, gender, "raw", 250);
-        console.log(data);
-
-        var y = d3.scaleLinear().domain([0, data[0][1]]).range([0 + margin_y, svgH - margin_y]);
-        var x = d3.scaleLinear().domain([0, data.length]).range([0 + margin_x, svgW - margin_x]);
-        // var x = d3.scaleOrdinal().band([0,svgW]);
-
-
-        //numbers on scale
-        g.selectAll('.yLabel').data(y.ticks(5)).enter().append('svg:text').attr('class','yLabel')
-        .text(String).attr('x',25).attr('y', function(d,i){return svgH - y(d)}).attr('text-anchor','end');
-
-        //%s on scale
-        g.selectAll('.yPercents').data(y.ticks(5)).enter().append('svg:text').attr('class','yPercents').text("%")
-        .attr('x',25).attr('y', function(d,i){return svgH - y(d)});
-
-        //horizonal grid lines
-        var yGrid = g.selectAll('.yGrid').data(y.ticks(5)).enter().append('svg:line').attr('class','yGrid')
-        .attr('x1', 0).attr('y1', function(d,i){return svgH - y(d)})
-        .attr('x2', svgW).attr('y2', function(d,i){return svgH - y(d)}).attr('stroke','gray');
-
-        //bars
-        var bar = g.selectAll('rect')
-        .data(data).enter().append('rect')
-        .attr('x',function(d,i){return x(i);})
-        .attr('y', function(d,i){return svgH - y(d[1]);}) //top of bars
-        .attr('height',function(d,i){return y(d[1]) - margin_y;}) //don't go past bottom margin
-        .attr('width', 20)
-        .style('fill','blue')
-        .style('opacity', 1)
-        .attr('class','bar');
-
-
-
-        //bar labels
-        var labels = g.selectAll(".bar").enter().data(data).enter().append('svg:text')
-        .text(function(d,i){return d[0];})
-        .attr('x',function(d,i){return x(i);})
-        .attr('y', function(d,i){return svgH - y(d[1]);})
-        // .attr('dx',function(d,i){return x(i);})
-        // .attr('dy', function(d,i){return svgH - y(d[1]);})
-        .attr("transform", function(d,i){return "rotate("+-25+","+x(i)+"," +(svgH - y(d[1]))+")";})
-        .attr('class','barlabel');
-        // g.selectAll('.barlabel').attr('transform','translate(20,0)');
-
-
-        // labels.attr('transform','translate(20,0)');
-        bar.attr('transform','translate(20,0)'); //push them right so scale numbers aren't on top
-        yGrid.attr('transform','translate(50,0)');
-        // g..attr('transform', 'rotate(30, 150, 100)');
-
-        //load gender alone data
-        testfile = statfiles.gender;
-        data = getStats1(testfile, "perc", 1);
-
-        //make translucent block showing the average
-        var avgBlock = g.append('rect').attr('x', 50).attr('y', y(testfile[gender]['perc']))
-        .attr('width', svgW).attr('height', y(testfile["Female"]['perc']) - margin_y)
-        .style('opacity', 0.5).style('fill','orange');
-    }
-
-    function firstTests(){
-        // var circ1 = circ(g, 40, 30, 15, 'blue', 'green');
-        // var rect1 = rect(g, 50, 100, 10, 100);
-
-        // rect1.attr('transform', 'translate(100, 60),scale(2, 1),rotate(30, 150, 100)');
-        // rect1.attr('transform', 'scale(2, 1)');
-        // rect1.attr('transform', 'rotate(30, 150, 100)');
-
-        //expanding circle
-        // circ1.transition().delay(100).duration(4000).attr('r',50).attr('cx', 90).style('fill','red');
-
-        //make shape functions
-        function circ(obj, cx, cy, r, fill, outline){
-            return obj.append('circle').attr('r',r).attr('cx', cx).attr('cy', cy).style('fill', fill).style('stroke',outline);
-        }
-
-        function rect(obj, x, y, w, h){
-            return obj.append('rect').attr('x', x).attr('y', y).attr('width', w).attr('height', h);
-        }
-
-
-        //bullet list
-        d3.select('body').append('ul');
-        d3.select('ul').selectAll('li').data(data).enter().append('li').text(function(d){
-            return(d[0] + ": " + d[1]);
-        });
-
-        // var selection = d3.selectAll("div");
-        // selection.text("hey");
-
-        // d3.selectAll("div")
-        // .text("yoyo")
-        // .style('text-align','left')
-        // .append("p")
-        // .text("hi");
-
-        // d3.select("div").append('p').text("yes");
-
-        // d3.select('body').insert('div', 'div:nth-child(3)').text("yee");
-
-        // console.log(d3.selectAll("div").text());
-        // d3.select("h1").attr('align',"center").html('<h2>heyguys</h2>');
-
-        // d3.select('body').html("<h1>fuck you</h1>");
-    }
-
-*/
-
-
-
-
-
-
-
-
 
     //for statfiles with only 1 group
     //outputs sorted array for chosen stat (raw, perc, whatever)
